1bf56874bc2b196dea00a221c3aa34c767e07ff7
Haileleul Menker <hailewolfe1189@gmail.com>
2026-09-15 18:04:17 +0300

    link to the pages

--- challenges/views.py
+++ challenges/views.py
@@ -1,12 +1,7 @@
 from calendar import month
-
+from django.urls import reverse
 from django.shortcuts import render
-from django.http import (
-    HttpRequest,
-    HttpResponse,
-    HttpResponseNotFound,
-    HttpResponseRedirect,
-)
+from django.http import HttpResponse, HttpResponseNotFound, HttpResponseRedirect
 
 monthly_challenges = {
     "january": "Hello, world. January challenges.",
@@ -25,20 +20,46 @@ monthly_challenges = {
 
 # Create your views here.
 
+def index(request):
+    list_items = ""
+    months = list(monthly_challenges.keys())
+
+    for month in months:
+        capitalize_month = month.capitalize()
+        month_path = reverse("month-challenge", args=[month])
+        list_items += f"<li><a href=\'{month_path}\'>{capitalize_month}</a></li>"
+
+    response_data = f"<ul>{list_items}</ul>"
+    return HttpResponse(response_data)
 
 def monthly_challenge_by_number(request, month):
     months = list(monthly_challenges.keys())
 
     if month > len(months):
-        return HttpResponseNotFound("This month is not supported.")
+        return HttpResponseNotFound("<h1>This month is not supported.</h1>")
 
-    forward_month = months[month - 1]
-    return HttpResponseRedirect("/challenges/" + forward_month)
+    redirect_month = months[month - 1]
+    redirect_path = reverse("month-challenge", args=[redirect_month])
+    return HttpResponseRedirect(redirect_path)
 
 
 def monthly_challenge(request, month):
     try:
         challenge_text = monthly_challenges[month]
-        return HttpResponse(challenge_text)
+        response_data = f"<h1>{challenge_text}</h1>"
+        return HttpResponse(response_data)
     except KeyError:
-        return HttpResponseNotFound("This month is not supported.")
+        return HttpResponseNotFound("<h1>This month is not supported.</h1>")
+    # except KeyError:
+    #     return HttpResponse("<h1>This month is not supported.</h1>", status=404)
+    # except KeyError:
+    #     response = HttpResponseNotFound("<h1>This month is not supported.</h1>")
+    #     print(response.status_code)
+    #     print(response["Content-Type"])
+    #     return response
+
+# except KeyError:
+#     response = HttpResponseNotFound("<h1>This month is not supported.</h1>")
+#     print(response.status_code)
+#     print(response["Content-Type"])
+#     return response
